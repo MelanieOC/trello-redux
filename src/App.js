8e74e6cd615de2d0f933922742495f696862939a
MelanieOC <melanie.ocharan@gmail.com>
2017-11-06 14:52:20 -0500

    componente signup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './trello-logo-white.svg';
+import { FormControl, FormGroup, Image, Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div align='center'>
+        <div style={{ width: '450px',marginTop:'20px' }}>
+          <Image src={logo} width='200px'/>
+          <form id='signUp'>
+            <FormGroup bsSize='large'>
+              <FormControl type="text" placeholder="First Name" />
+            </FormGroup>
+            <FormGroup bsSize="large">
+              <FormControl type="text" placeholder="Last Name" />
+            </FormGroup>
+            <FormGroup>
+              <FormControl type="text" placeholder="Email" />
+            </FormGroup>
+            <FormGroup bsSize='large'>
+              <FormControl type="text" placeholder="Password" />
+            </FormGroup>
+            <FormGroup bsSize='large'>
+              <FormControl type="text" placeholder="Confirm password" />
+            </FormGroup>
+            <Button type='submit'>Sign Up</Button>
+          </form>
+          <a href='#'>Sign in</a>
+        </div>
       </div>
     );
   }
 }
+//<NavLink to='#'>Sign in</NavLink>
 
 export default App;
