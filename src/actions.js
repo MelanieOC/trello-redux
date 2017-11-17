a172f333d174af29be0c1602417a98bff503b897
MelanieOC <melanie.ocharan@gmail.com>
2017-11-17 13:38:51 -0500

    probando firebase

--- src/actions.js
+++ src/actions.js
@@ -1,39 +1,55 @@
 import store from './store';
-//import firebase from 'firebase';
+import firebase from 'firebase';
 
-export const addBoard = (value)=>{
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyASoVpVqUcagSmBBDoC0ABtlmOXrmBtuT8",
+    authDomain: "trello-fe8ac.firebaseapp.com",
+    databaseURL: "https://trello-fe8ac.firebaseio.com",
+    projectId: "trello-fe8ac",
+    storageBucket: "trello-fe8ac.appspot.com",
+    messagingSenderId: "579542162339"
+};
+firebase.initializeApp(config);
+
+export const addBoard = (value) => {
     let boards = [...store.getState().boards];
     console.log(value);
     boards.push({
         name: value,
-        tarjetas:[]
+        tarjetas: []
     })
+    let newBoard = {
+        tarjetas: []
+    }
+    firebase.database().ref('user/' + value).set(newBoard);
     store.setState({
         boards: boards
     })
+
 }
-export const addList = (value,list)=>{
+export const addList = (value, list) => {
     let boards = [...store.getState().boards];
-    let newList = boards.map(a=>{
-        if(a.name===list.name){
-            a.tarjetas.push({card:value, stages:[]});
+    let newList = boards.map(a => {
+        if (a.name === list.name) {
+            a.tarjetas.push({ card: value, stages: [] });
         }
         return a;
     })
     store.setState({
         boards: newList
     })
-    
+
 }
-export const addCard = (value,list,card)=>{
+export const addCard = (value, list, card) => {
     let boards = [...store.getState().boards];
-    let newList = boards.map(a=>{
-        if(a.name===list.name){
-            a.tarjetas.map(b=>{
-               if(b.card===card){
-                   b.stages.push(value);
-               }
-               return b;
+    let newList = boards.map(a => {
+        if (a.name === list.name) {
+            a.tarjetas.map(b => {
+                if (b.card === card) {
+                    b.stages.push(value);
+                }
+                return b;
             });
         }
         return a;
