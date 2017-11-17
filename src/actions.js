0e2eca67fd8298d4a0a409c37736fb0f1a304321
MelanieOC <melanie.ocharan@gmail.com>
2017-11-17 17:40:09 -0500

    prueba sign up

--- src/actions.js
+++ src/actions.js
@@ -59,3 +59,67 @@ export const addCard = (value, list, card) => {
     firebase.database().ref(user + '/boards/' + list.id + '/tarjetas').set(newList).then(() => console.log('sip'));
 
 }
+export function signUp (fullname, email, pass, survey, question, options) 
+{
+   console.log ('signUp' + fullname + email + pass);
+
+   firebase.auth().createUserWithEmailAndPassword (email, pass).then ( user => {
+      let newuser = {
+         fullname, email, survey, question, options
+      }
+      firebase.database().ref ('users/' + user.uid).set (newuser);   
+
+     // firebase.database().ref ('users/' + user.uid + '/options').update ( 'option1, option2, option3...');   
+     //  firebase.database().ref ('users/').push (newuser);   
+      
+      firebase.database().ref ('users/' + user.uid).once ('value').then ( res => {
+         const fullUserInfo = res.val(); 
+
+         console.log ('full info ', fullUserInfo);
+         store.setState ( {
+            user: {
+               id : user.uid,
+               email :  fullUserInfo.email,
+               fullname :  fullUserInfo.fullname,
+               survey :  fullUserInfo.survey,
+               question :  fullUserInfo.question,
+               options :  fullUserInfo.options               
+            }
+         })
+      })
+
+   })
+   
+}
+
+export function signOut () {
+   firebase.auth().signOut();
+   store.setState ( {
+      successLogin : false,
+      user: {
+         id :'',
+         email :  ''
+      }
+   })
+}
+
+export function signIn (user, pass) {
+   firebase.auth().signInWithEmailAndPassword(user, pass).then (userObj => {
+
+      firebase.database().ref ('users/' + userObj.uid).once ('value').then ( res => {
+         const fullUserInfo = res.val(); 
+
+         console.log ('full info ', fullUserInfo);
+         store.setState ( {
+            user: {
+               id : userObj.uid,
+               email :  fullUserInfo.email,
+               fullname :  fullUserInfo.fullname,
+               survey :  fullUserInfo.survey,
+               question :  fullUserInfo.question,
+               options :  fullUserInfo.options               
+            }
+         })
+      })
+   })
+}
