b205b3ec5a8124192627f761bec4ab8052cbe3f6
MelanieOC <melanie.ocharan@gmail.com>
2017-11-17 01:14:10 -0500

    agrega list

--- src/actions.js
+++ src/actions.js
@@ -18,8 +18,15 @@ export const addList = (value,list)=>{
     console.log(value);
     
     let boards = [...store.getState().boards];
-    let newList = boards.filter(a=>a.name===list.name);
-    console.log(newList);
+    let newList = boards.map(a=>{
+        if(a.name===list.name){
+            a.tarjetas.push({card:value, stages:[]});
+        }
+        return a;
+    })
+    store.setState({
+        boards: newList
+    })
     
 }
 export const addCard = (value,card)=>{
